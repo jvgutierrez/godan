afc661996904a2b8fb857540a0266ba2442a601e
Valentin Gutierrez <jv.gutierrezb@gmail.com>
2016-06-04 10:21:24 +0200

    Apply shellcheck fixes

--- docker/run_containers.sh
+++ docker/run_containers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ $1 == "es" ] || [ $1 == "mysql" ]
+if [ "$1" == "es" ] || [ "$1" == "mysql" ]
 then
-	cp server/godanserver.toml.${1} server/godanserver.toml
-	cp docker-compose-${1}.yml docker-compose.yml
+	cp server/godanserver.toml."${1}" server/godanserver.toml
+	cp docker-compose-"${1}".yml docker-compose.yml
 	docker-compose up -d
 else
 	echo "Usage: $0 es|mysql"
