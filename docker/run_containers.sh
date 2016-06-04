9dbc8f7e1ed8dabb36b337c515cf8ce5ff1fb39c
Valentin Gutierrez <jv.gutierrezb@gmail.com>
2016-06-04 10:22:55 +0200

    Let docker-compose manage building the containers

--- docker/run_containers.sh
+++ docker/run_containers.sh
@@ -3,7 +3,7 @@ if [ "$1" == "es" ] || [ "$1" == "mysql" ]
 then
 	cp server/godanserver.toml."${1}" server/godanserver.toml
 	cp docker-compose-"${1}".yml docker-compose.yml
-	docker-compose up -d
+	docker-compose build && docker-compose up -d
 else
 	echo "Usage: $0 es|mysql"
 fi
